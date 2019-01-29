0c5ffc27c7ec62981e8bd6917f6cbee6a34d46ee
kashi992 <45970492+kashi992@users.noreply.github.com>
2019-01-29 16:18:37 +0500

    Initial commit

---
+++ assets/js/app/app.js
@@ -0,0 +1,5 @@
+var app = angular.module("DDApp", ['ngIntlTelInput', 'ngResource',
+    'ui.router', 'satellizer', 'ngFileUpload', 'ui.bootstrap',
+    'rx','ui.date', 'ngTagsInput', 'textAngular','angucomplete-alt',
+    'ngStorage', 'awesome-rating', 'oc.lazyLoad',
+    'angularUtils.directives.dirPagination', 'angular-img-cropper', 'ui.bootstrap.pagination']);
